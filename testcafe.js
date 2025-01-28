fabf1a023aa7b9bc7a61c6a56b25f288ddaae2cc
AthanasiosKatsikis_bi64sm <gogs@fake.local>
2025-01-28 03:16:49 +0200

    Added Tests

---
+++ testcafe.js
@@ -0,0 +1,26 @@
+import { Selector } from 'testcafe';
+
+fixture('Student Manager App Client Side Test')
+    .page('file:///home/athanasioskatsikisbi64sm/Desktop/repos/StudentManagerApp/client/index.html');  // Ensure your app is running
+
+
+test('Client Can Add A Student And See It At The Bottom Of The List', async (t) => {
+    const studentList = Selector('#studentList li');
+    
+    await t
+        .typeText('#name', 'Max Newman')
+        .typeText('#age', '22')
+        .typeText('#gender', 'Male')
+        .click('button[type="submit"]')
+        .wait(1000); 
+
+    await t
+        .scrollIntoView(studentList.nth(0))  
+        .wait(500); 
+    
+    const addedStudent = studentList.withText('Max Newman');
+    await t.expect(addedStudent.exists).ok('Max Newman Was Not Added Successfully');
+    
+    await t.scrollIntoView(studentList.withText('Max Newman'))
+    .wait(5000); 
+});
